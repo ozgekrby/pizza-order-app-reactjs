d965cea9a274bcff77c04ac234c20dcc2f232d56
ozge <karabayozge@gmail.com>
2024-09-12 17:46:43 +0300

    css header location fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "/src/App.css"
 import { useLocation } from 'react-router-dom';
 export default function Header() {
   const location = useLocation();
-  const headerClass = (location.pathname === '/' || location.pathname === '/success' || location.pathname==="/home") ? 'header-home' : 'header-default';
+  const headerClass = (location.pathname === "/siparis-olustur" ) ? 'header-default' : 'header-home';
   return (
     <div className={headerClass}>
       <img src="Assets/Iteration-1-assets/logo.svg" alt="" />
